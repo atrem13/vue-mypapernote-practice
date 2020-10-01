d691d47c906176fbbb720892c84427e76f9594e0
atrem13 <mertayoga17.my@gmail.com>
2020-10-01 23:47:13 +0800

    fix some bug in your code

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <template>
   <div>
-    <!-- coba sweet alert -->
-    <!-- <SweetAlertApp></SweetAlertApp> -->
     <ToastedApp></ToastedApp>
   </div>
 </template>
 
 <script>
-// import SweetAlertApp from './components/SweetAlertApp'
 import ToastedApp from './components/ToastedApp'
 export default {
   components: {
-    // SweetAlertApp
     ToastedApp
   }
 }
